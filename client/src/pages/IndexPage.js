0a856252ef8d05a7cd110bfcb20474e6c5073b0d
Sudershan-18 <77936330+Sudershan-18@users.noreply.github.com>
2024-03-15 16:46:41 +0530

    Update IndexPage.js

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -4,11 +4,7 @@ import {useEffect, useState} from "react";
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
   useEffect(() => {
-<<<<<<< HEAD
-    fetch('https://blogosphere-blog-app-omb2.vercel.app/post').then(response => {
-=======
     fetch('https://blogosphere-blog-app.onrender.com/post').then(response => {
->>>>>>> 85136fdfcd3dc52559381e7a13bb598a7f79d78d
       response.json().then(posts => {
         setPosts(posts);
       });
