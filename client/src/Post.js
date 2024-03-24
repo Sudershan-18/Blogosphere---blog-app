e5ae195c8b2311795583ed142161ac222a16562e
Sudershan-18 <77936330+Sudershan-18@users.noreply.github.com>
2024-03-25 00:14:54 +0530

    render

--- client/src/Post.js
+++ client/src/Post.js
@@ -6,7 +6,7 @@ export default function Post({_id, title, summary, cover, content, createdAt, au
         <div className="post">
             <div className="image">
                 <Link to={`/post/${_id}`}>
-                <img src={'https://blogosphere-blog-app-dnju.vercel.app/'+cover} alt="" />
+                <img src={'https://blogosphere-blog-app.onrender.com/'+cover} alt="" />
                 </Link>
             </div>
             <div className="texts">
