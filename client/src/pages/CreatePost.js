f84fea15352c555e610b39eff4d7b9a8fc7710ff
Sudershan-18 <77936330+Sudershan-18@users.noreply.github.com>
2024-03-25 00:15:23 +0530

    render

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -20,7 +20,7 @@ export default function CreatePost() {
 
         ev.preventDefault();
         
-        const response = await fetch('https://blogosphere-blog-app-dnju.vercel.app/post', {
+        const response = await fetch('https://blogosphere-blog-app.onrender.com/post', {
             method: 'POST',
             // in body we can send the info through json but since we have a file so we will send it through a form
             body: data,
